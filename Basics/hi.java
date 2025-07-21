98332cceea902aa23dcf275b456dc4a00aff67d5
kavin766 <codehub676@gmail.com>
2025-07-21 22:27:35 +0530

    hiii_summatha

---
+++ Basics/hi.java
@@ -0,0 +1,9 @@
+public class hi {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+        }
+        System.out.println("End of program.");
+    }
+}
